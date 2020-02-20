da00e8d26e818f7ee88c7377c8ccc839388eb395
Alejandro Estaban Lopez <aesteban@f-l3210-pc14.aulas.etsit.urjc.es>
2020-02-20 10:49:23 +0100

    Probando javascript

---
+++ P2/L4/ejemplo2.js
@@ -0,0 +1,24 @@
+console.log("Ejecutando JS...");
+
+const test = document.getElementById('test');
+const boton1 = document.getElementById('boton1');
+const boton2 = document.getElementById('boton2');
+const n1 = 1;
+const n2 = 2;
+
+
+test.onclick = () =>{
+  console.log("Clickk!!!");
+  //Si no tiene color al pinchar lo ponemos al amarillo y si pinchamos otra vez lo quitamos
+  if (test.style.backgroundColor== ""){
+    test.style.backgroundColor = "yellow";
+  }else {
+    test.style.backgroundColor = "";
+  }
+}
+
+boton1.onclick =() =>{
+  console.log("BOTON PULSADO");
+  test.innerHTML = "CAMBIADO POR EL BOTTON" + "<br>" + n1.valueOf();
+
+}
---
+++ P2/L4/hello.js
@@ -0,0 +1,5 @@
+//1 COMENTARIO DE UNA SOLA LINEA
+/* Comentario de bloque
+*/
+
+console.log("Archivo javascript!!");
