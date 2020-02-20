9ad17598aa8c41a1ab3975d8b2fda686ac622dc3
AlejandroEsLo <alex-lojim@hotmail.com>
2020-02-20 20:21:15 +0100

    Calculadora avance, falta concretar operaciones

--- P2/calc.js
+++ P2/calc.js
@@ -1,9 +1,50 @@
 console.log("Ejecutando JS...");
 
-const boton1 = document.getElementById('boton1');
 const resultado = document.getElementById('resultado');
+var resultado_final = 0;
 
-boton1.onclick =() =>{
+const b1 = document.getElementById('b1');
+const b2 = document.getElementById('b2');
+const boton_suma = document.getElementById('boton_suma');
+const boton_igual = document.getElementById('boton_igual');
+
+b1.onclick =() =>{
   console.log("BOTON1 PULSADO");
-  resultado.innerHTML += "(";
+  if (resultado.innerHTML == 0 ||resultado.innerHTML == "+"){
+      resultado.innerHTML = 1;
+  }else {
+    resultado.innerHTML += 1;
+  }
+  resultado_final = resultado.innerHTML;
+  console.log("resultado final: " + resultado_final);
+  console.log("resultado: " + resultado.innerHTML);
+}
+
+b2.onclick =() =>{
+  console.log("BOTON2 PULSADO");
+  if (resultado.innerHTML == 0 ||resultado.innerHTML == "+"){
+      resultado.innerHTML = 2;
+  }else {
+    resultado.innerHTML += 2;
+  }
+  resultado_final = resultado.innerHTML;
+  console.log("resultado final: " + resultado_final);
+  console.log("resultado: " + resultado.innerHTML);
+}
+
+boton_suma.onclick =() =>{
+  console.log("SUMA PULSADO");
+  resultado.innerHTML = "+";
+  resultado_final = resultado.innerHTML + resultado_final;
+  console.log("resultado final: " + resultado_final);
+  console.log("resultado: " + resultado.innerHTML);
+
+}
+
+boton_igual.onclick =() =>{
+  console.log("IGUAL PULSADO");
+  resultado.innerHTML = resultado_final;
+  console.log("resultado final: " + resultado_final);
+  console.log("resultado: " + resultado.innerHTML);
+
 }
