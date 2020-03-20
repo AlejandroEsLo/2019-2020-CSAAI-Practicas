bd197f130843bfd80a4575f7d6d2b4afcb5ee616
AlejandroEsLo <alex-lojim@hotmail.com>
2020-03-20 21:10:34 +0100

    Mi pong con clase bola y raqueta!!

--- P3/pong.js
+++ P3/pong.js
@@ -10,151 +10,17 @@ console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
 //-- Obtener el contexto para pintar en el canvas
 const ctx = canvas.getContext("2d");
 
-// <<<<< Objeto: Bola >>>>>//
-
-const bola = {
-
-  //-- Constante: Tamaño de la bola
-  size : 5,
-
-  //-- Contante: Posicion inicial de la bola
-  x_ini : 100,
-  y_ini : 200,
-
-  //-- Posicion generica de la bola
-  x : 0,
-  y : 0,
-
-  //-- Velocidad inicial de la bola
-  vx_ini : 6,
-  vy_ini : 3,
-
-  //-- Velocidad genérica de la bola
-  //-- Inicialmente a cero
-  vx : 0,
-  vy : 0,
-}
-
-// <<<<< Funciones Bola >>>>>//
-function bola_draw()
-{
-  //----- Dibujar la Bola
-  ctx.beginPath();
-  ctx.fillStyle='white';
-
-  //-- x,y, anchura, altura
-  ctx.rect(bola.x, bola.y, bola.size, bola.size);
-  ctx.fill();
-}
-
-function bola_init()
-{
-  //-- Inicializa la bola: A su posicion inicial
-  bola.x = bola.x_ini;
-  bola.y = bola.y_ini;
-}
-
-function bola_update()
-{
-  bola.x += bola.vx;
-  bola.y += bola.vy;
-}
-
-// <<<<< Objeto: raqueta izquierda >>>>>//
-const raqI = {
-  //-- Constante: Tamaño de la raqueta
-  width : 10,
-  height: 40,
-
-  //-- Constante: Posicion inicial
-  x_ini : 50,
-  y_ini : 100,
-
-  //-- Constante: Velocidad
-  v_ini : 3,
-
-  //-- Velocidad (variable)
-  v : 0,
-}
-
-// <<<<< Funciones Raqueta izquierda >>>>>//
-function raqI_init()
-{
-  raqI.x = raqI.x_ini;
-  raqI.y = raqI.y_ini;
-}
-
-function raqI_update()
-{
-  raqI.y += raqI.v;
-}
-
-function raqI_draw()
-{
-  //------- Dibujar las raquetas
-  ctx.beginPath();
-  ctx.fillStyle='white';
-
-  //-- Raqueta izquierda
-  ctx.rect(raqI.x, raqI.y, raqI.width, raqI.height);
-
-  //-- Pintar!
-  ctx.fill();
-}
-
-// <<<<< Objeto: raqueta derecha >>>>>//
-const raqD = {
-  //-- Constante: Tamaño de la raqueta
-  width : 10,
-  height: 40,
-
-  //-- Constante: Posicion inicial
-  x_ini : 540,
-  y_ini : 300,
-
-  //-- Constante: Velocidad
-  v_ini : 3,
-
-  //-- Velocidad (variable)
-  v : 0,
-}
-
-// <<<<< Funciones Raqueta derecha >>>>>//
-function raqD_init()
-{
-  raqD.x = raqD.x_ini;
-  raqD.y = raqD.y_ini;
-}
-
-function raqD_update()
-{
-  raqD.y += raqD.v;
-}
-
-function raqD_draw()
-{
-  //------- Dibujar las raquetas
-  ctx.beginPath();
-  ctx.fillStyle='white';
-
-  //-- Raqueta izquierda
-  ctx.rect(raqD.x, raqD.y, raqD.width, raqD.height);
-
-  //-- Pintar!
-  ctx.fill();
-}
-
 //-- Pintar todos los objetos en el canvas
 function draw() {
 
   //----- Dibujar la Bola
-  bola_draw();
+  bola.draw();
 
   //-- Dibujar la raqueta izquierda
-  raqI_draw();
+  raqI.draw();
 
   //-- Dibujar la raqueta derecha
-  raqD_draw();
+  raqD.draw();
 
   //--------- Dibujar la red
   ctx.beginPath();
@@ -186,8 +52,8 @@ function animacion()
 // <<<<< Actualizar las posiciones de los objetos móviles >>>>>//
 
   //-- Actualizar las raquetas con la velocidad actual
-  raqI_update();
-  raqD_update();
+  raqI.update();
+  raqD.update();
 
 // <<<<< Comprobar colisiones canvas >>>>>//
 
@@ -228,6 +94,8 @@ function animacion()
   if (bola.x >= raqI.x && bola.x <=(raqI.x + raqI.width) &&
       bola.y >= raqI.y && bola.y <=(raqI.y + raqI.height)) {
 
+      bola.vx = bola.vx * -1;
+/*
       //Controlar velocidad bola respecto la raqueta
       if(raqI.v == 0){//Raqueta parada
         bola.vx = bola.vx * -1;
@@ -235,7 +103,7 @@ function animacion()
           bola.vx = bola.vx * -1;
           bola.vy = bola.vy + 2;
       }
-
+*/
   }
 
   //-- Comprobar si hay colisión con la raqueta derecha
@@ -247,7 +115,7 @@ function animacion()
 // <<<<< Actualizar posicion bola >>>>>//
 
   //-- Actualizar coordenada x de la bola, en funcion de su velocidad
-    bola_update()
+    bola.update()
 
 // <<<<< Borrar la pantalla y Dibujar >>>>>//
 
@@ -258,12 +126,20 @@ function animacion()
   draw();
 }
 
-//-- Inicializa la bola: A su posicion inicial
-bola_init();
+// <<<<< Objeto: Bola >>>>>//
+//-- Inicializa la bola: Llevarla a su posicion inicial
+const bola = new Bola(ctx);
 
-//-- Inicializar las raquetas a su posicion inicial
-raqI_init();
-raqD_init();
+//-- Crear las raquetas
+// <<<<< Objeto: raqueta izquierda >>>>>//
+const raqI = new Raqueta(ctx);
+// <<<<< Objeto: raqueta derecha >>>>>//
+const raqD = new Raqueta(ctx);
+
+//-- Cambiar las coordenadas de la raqueta derecha
+raqD.x_ini = 540;
+raqD.y_ini = 300;
+raqD.init();
 
 //-- Arrancar la animación
 setInterval(()=>{
@@ -291,7 +167,7 @@ window.onkeydown = (e) => {
 
     case " ":
       //-- Llevar bola a su posicion incicial
-      bola_init();
+      bola.init();
       //-- Darle velocidad
       bola.vx = bola.vx_ini;
     default:
